2ded8f318090c8a29dda5d223af9fb0f14a06b50
Pratik Ghadge <pratikghadge.cs@gmail.com>
2026-01-12 08:42:33 +0530

    Add filename validation

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from file_analyzer import analyze_file
 
 app = Flask(__name__)
 
+def is_valid_filename(filename):
+    if not filename.endswith(".txt"):
+        return "INVALID_EXTENSION"
+    if ".." in filename or "/" in filename or "\\" in filename:
+        return "INVALID_PATH"
+    
+    return None
+
 @app.route("/analyze", methods=["POST"])
 def analyze():
     
@@ -18,6 +26,20 @@ def analyze():
         return jsonify({
             "status": "error",
             "message": "file name is required"
+        }), 400
+    
+    validation_error = is_valid_filename(filename)
+
+    if validation_error == "INVALID_EXTENSION":
+        return jsonify({
+            "status": "error",
+            "message": "only .txt files are allowed"
+        }), 400
+    
+    if validation_error == "INVALID_PATH":
+        return jsonify({
+            "status": "error",
+            "message": "file should be present in the same directory"
         }), 400
     
     output = analyze_file(filename)
